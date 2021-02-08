715be85b79415a36b992229cac8d297a6b122657
FelipeDominguesB <felipedom2001@hotmail.com>
2021-02-08 17:12:26 -0300

    Consegui realizar meu objetivo primário, adicionar imagens de maneira assíncrona, objetivo concluido e repositório sendo deixado de lado até segunda ordem

--- Promisses.js
+++ Promisses.js
@@ -9,9 +9,11 @@ class Promisses{
     {
         let btnMessage = document.querySelector('#btnMessage');
         let btnMath = document.querySelector('#btnMath');
+        let btnFile = document.getElementById('fileInput');
 
         let input1 = document.getElementById('value1');
         let input2 = document.getElementById('value2');
+        
         let resultado = document.getElementById('result');
 
         btnMessage.addEventListener('click', () =>{
@@ -26,9 +28,32 @@ class Promisses{
                 this.DoOperation(value, input2.value);
             });
         });
-            
+
+        
+        btnFile.addEventListener('change', (event) =>{
+            this.GetPhotos(event).then((filesArray) => {
+                this.ShowArray(filesArray);
+            });
+        });
     }
 
+
+    ShowArray(files)
+    {
+        return new Promise((resolve, reject) =>{
+
+            files.forEach(element => {
+                let th = document.createElement('td');
+                th.style.display = 'block';
+                let imagem = document.createElement('video');
+                imagem.src = element;
+                imagem.className = 'mediaSize';
+                th.appendChild(imagem);
+                document.querySelector('tr').appendChild(th);
+            });
+            
+        });
+    }
     ShowMessage()
     {
         return new Promise((resolve, reject) =>{
@@ -68,4 +93,48 @@ class Promisses{
         }); 
     }
 
+    GetPhotos(event)
+    {
+
+        return new Promise((resolve, reject) =>
+        {
+            let photos = event.target.files;
+            let fileArray = []; 
+            if(photos)
+            {
+
+                for(let i = 0; i < photos.length; i++)
+                {
+                    let fileReader = new FileReader();
+                    
+                    fileReader.onload = () =>{
+                        let fileType = fileReader.result.slice(0, 10);
+        
+                        switch(fileType)
+                        {
+                            case 'data:video':
+                                
+                                fileArray.push(fileReader.result);
+
+                                console.log(photos.length);
+                                console.log(fileArray.length);
+
+                                if(photos.length == fileArray.length)
+                                {
+                                    resolve(fileArray);
+                                }
+                            break;
+
+                            default: break;
+                        }
+                    }
+                    fileReader.readAsDataURL(photos[i]);
+                }
+            }
+
+            console.log('Sai da primeira promisse');
+            
+        });
+       
+    }
 }
